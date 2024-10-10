feecfc3425068b8d21b79d72ceab1186ba8c7330
heejae <leeheejaelcode@gmail.com>
2024-10-10 11:15:32 +0900

    [학습] 16. import&export.ts
    - type import&export 문법

--- src/16.import&export.ts
+++ src/16.import&export.ts
@@ -2,6 +2,11 @@
 /*                              import export 문법                              */
 /* -------------------------------------------------------------------------- */
 
+// import 시에는 tsconfig.ts의 module 방식을 ESNext로 변경
+// package.json에도 {"type": "module"}로 변경
+// index.html에 스크립트의 type을 module로 변경
+// js파일 확장자를 mjs로 변경
+
 import { Cars, Bike, fnType, Dog } from "./type";
 
 let 자동차: Cars = {
